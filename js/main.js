1d1d2c66970d2ac7456d02321c659ff2e7022579
Cathy <smile25896@gmail.com>
2021-07-07 22:55:33 +0800

    新增：「快速下單區」與「好團熱銷必買」區塊

--- js/main.js
+++ js/main.js
@@ -2,4 +2,26 @@ $(document).ready(function(){
     $('.single-item').slick({
         autoplay: true
     });
-  });
+
+    $('.group-buying-box').slick({
+        autoplay: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 1220,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              }
+            },
+            {
+              breakpoint: 840,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+          ]
+    });
+});
